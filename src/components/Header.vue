6aee43cf963049b55d5b00fe466e5a318128dfa4
Naoki0722 <mattu.nao722@gmail.com>
2022-04-10 19:06:27 +0900

    refactoring code

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,40 +1,25 @@
 <template>
   <div>
-    <v-app-bar color="deep-purple accent-4" dark>
-
-      <v-toolbar-title>
-        <v-img src="../assets/header-logo.png"   max-height="100" max-width="250"></v-img>
-      </v-toolbar-title>
-
+    <v-app-bar color="deep-purple" dark>
+      <v-toolbar-title>USER-APP</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-menu offset-y v-if="parentData">
-        <template v-slot:activator="{ on, attrs }">
-          <v-btn
-            color="deep-purple accent-2"
-            dark
-            v-bind="attrs"
-            v-on="on"
-          >
-            MENU
-          </v-btn>
-        </template>
-        <v-list>
-          <v-list-item link>
-            <v-list-item-title @click="$router.push('/')">ログイン</v-list-item-title>
-          </v-list-item>
-          <v-list-item link>
-            <v-list-item-title @click="$router.push('/signup')">新規登録</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
-      <v-btn  v-else
-        color="deep-purple accent-2"
-        dark
-        @click="auth"
-      >ログアウト</v-btn>
+      <v-app-bar-nav-icon @click="drawer = true" ml="10"></v-app-bar-nav-icon>
     </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary right>
+      <v-list>
+        <v-list-item link>
+          <v-list-item-title @click="$router.push('/')"
+            >ログイン</v-list-item-title
+          >
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-title @click="$router.push('/signup')"
+            >新規登録</v-list-item-title
+          >
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
   </div>
-
 </template>
 
 <script>
@@ -42,55 +27,14 @@ export default {
   props: ["parentData"],
   data() {
     return {
-      isActive: false,
+      drawer: false,
+      group: null,
     };
   },
   methods: {
     auth() {
       this.$store.dispatch('logout');
-    }
-  }
+    },
+  },
 };
 </script>
-
-<style scoped>
-#header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-}
-
-#header,
-.header-logo,
-.header-link {
-  background-color:#64B5F6;
-}
-
-.header-logo img {
-  width: 40%;
-}
-
-.header-link {
-  display: flex;
-}
-
-.header-link p {
-  border: 1px solid gray;
-  border-radius: 10px;
-  padding: 10px;
-  margin: 15px;
-  background-color: white;
-  color: rgb(179, 177, 177);
-  font-weight: bold;
-}
-
-.header-link p:last-child {
-  margin-right: 30px;
-}
-
-.header-link p:hover {
-  cursor: pointer;
-  color: rgb(55, 55, 235);
-  transition: color .3s;
-}
-</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,29 +1,51 @@
 <template>
   <div>
-    <Header :parentData="sendFlag"/>
-    <v-card elevation="1"  width="40%" class="mx-auto mt-12">
-      <v-row justify="center">
-        <v-card-title>
-          <h1 class="display-1">ログイン</h1>
-        </v-card-title>
-      </v-row>
-      <v-card-text>
-        <v-form>
-          <v-text-field prepend-icon="mdi-account-circle" label="メールアドレス" v-model="email" />
-          <v-text-field prepend-icon= "mdi-lock" type="password" label="パスワード" v-model="password"/>
-          <v-row justify="center">
-            <v-card-actions>
-              <v-btn rounded v-ripple="{ center: true }" class="success" @click="auth">ログイン</v-btn>
-            </v-card-actions>
-          </v-row>
-        </v-form>
-      </v-card-text>
+    <v-card class="mx-auto overflow-hidden" height="100vh">
+      <Header :parentData="sendFlag" />
+      <v-card
+        elevation="1"
+        :width="$vuetify.breakpoint.xs ? '80%' : '50%'"
+        class="mx-auto mt-12"
+      >
+        <v-row justify="center">
+          <v-card-title>
+            <h1 class="display-1">ログイン画面</h1>
+          </v-card-title>
+        </v-row>
+        <v-card-text>
+          <v-form>
+            <v-text-field
+              prepend-icon="mdi-account-circle"
+              label="メールアドレス"
+              v-model="email"
+              class="py-5"
+            />
+            <v-text-field
+              prepend-icon="mdi-lock"
+              type="password"
+              label="パスワード"
+              v-model="password"
+              class="py-5"
+            />
+            <v-row justify="center">
+              <v-card-actions>
+                <v-btn
+                  x-large
+                  class="success mt-10 mb-5 text-h6 font-weight-bold"
+                  @click="auth"
+                  >ログイン</v-btn
+                >
+              </v-card-actions>
+            </v-row>
+          </v-form>
+        </v-card-text>
+      </v-card>
     </v-card>
   </div>
 </template>
 
 <script>
-import Header from '../components/Header'
+import Header from '../components/Header';
 export default {
   data() {
     return {
@@ -33,40 +55,17 @@ export default {
     };
   },
   components: {
-    Header
+    Header,
   },
   methods: {
     auth() {
       this.$store.dispatch('login', {
         email: this.email,
-        password: this.password
+        password: this.password,
       });
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
-<style scoped>
-#login {
-  text-align: center;
-}
-
-.content {
-  border: 1px solid black;
-  border-radius: 10px;
-  display: inline-block;
-  padding: 25px 100px;
-  margin: 200px auto 0;
-}
-
-.content h2 {
-  font-size: 40px;
-  margin: 10px 0 40px;
-}
-
-.content input {
-  display: block;
-  height: 25px;
-  margin: 20px 0;
-}
-</style>
+<style scoped></style>
